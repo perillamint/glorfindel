e1a6c77403b83cf0412c6cbe77eaef4f8f6f063e
perillamint <perillamint@quendi.moe>
2018-05-22 21:29:28 +0900

    Load config.yaml

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const fs = require('fs');
 const net = require('net');
 const tls = require('tls');
 const { Duplex } = require('stream');
+const yaml = require('js-yaml');
 
 const TLSTester = require('./tlstester.js');
+
+/* eslint-disable no-sync */
+const config = fs.readFileSync('config.yaml');
+/* eslint-enable no-sync */
 
 const tlsTester = new TLSTester({
     target: {
